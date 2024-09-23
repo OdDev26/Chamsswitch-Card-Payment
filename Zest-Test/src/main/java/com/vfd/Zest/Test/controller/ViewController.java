02f01a1d50bb8045828f6eea8d3ee4286a794dda
Michael Egbe-iyon <odmichael26@gmail.com>
2024-09-23 14:17:04 +0100

    card payment

--- Zest-Test/src/main/java/com/vfd/Zest/Test/controller/ViewController.java
+++ Zest-Test/src/main/java/com/vfd/Zest/Test/controller/ViewController.java
@@ -25,17 +25,17 @@ public class ViewController {
    @Value(value = "${vfd.token}")
    String vfdToken;
 
-
+   @GetMapping("/")
+   public String index(Model model) throws JsonProcessingException {
+      return "index";
+   }
    @GetMapping("/zest-payment")
    public String nonTokenizedPayment(Model model) throws JsonProcessingException {
       PaymentRequest paymentRequest = new PaymentRequest();
       model.addAttribute("paymentRequest",paymentRequest);
       return "non-tokenized-payment-form";
    }
-   @GetMapping("/")
-   public String index(Model model) throws JsonProcessingException {
-      return "index";
-   }
+
 
    @GetMapping("/tokenized")
    public String tokenizedPayment(Model model) throws JsonProcessingException {
